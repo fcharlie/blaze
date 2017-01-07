fd13fcd9e7cfbe6a9e52a4f044bc4d70cf9423ba
Force Charlie <charlieio@outlook.com>
2017-01-07 22:10:13 +0800

    update project file

--- tools/blaze/rar.cpp
+++ tools/blaze/rar.cpp
@@ -1,5 +1,9 @@
 #include "blaze.hpp"
+#include "unrar.h"
 
 bool RarDecompressArchive(const std::wstring &file, const std::wstring &dir) {
+	RAROpenArchiveDataEx rarArchiveEx;
+	memset(&rarArchiveEx, 0, sizeof(RAROpenArchiveDataEx));
+	//rarArchiveEx.ArcNameW = (wchar_t *)file.data();
 	return true;
 }

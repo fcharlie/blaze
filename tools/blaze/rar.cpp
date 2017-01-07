a77bf94c8f1e1df239d3466900675c8fac1377ab
Force Charlie <charlieio@outlook.com>
2017-01-07 22:53:20 +0800

    update unrar dll.def, export RARProcessFileW

--- tools/blaze/rar.cpp
+++ tools/blaze/rar.cpp
@@ -4,6 +4,8 @@
 bool RarDecompressArchive(const std::wstring &file, const std::wstring &dir) {
 	RAROpenArchiveDataEx rarArchiveEx;
 	memset(&rarArchiveEx, 0, sizeof(RAROpenArchiveDataEx));
+	RARProcessFileW(nullptr, 0, nullptr, nullptr);
+	//RARProcessFile(nullptr, 0, nullptr, nullptr);
 	//rarArchiveEx.ArcNameW = (wchar_t *)file.data();
 	return true;
 }
